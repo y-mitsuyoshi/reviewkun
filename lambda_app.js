2329038e7d4dc906d198b489426ca12cc2721a87
y-mitsuyoshi <glorymanchesteru@gmail.com>
2022-08-02 10:36:09 +0900

    add active user check

--- lambda_app.js
+++ lambda_app.js
@@ -11,17 +11,27 @@ const app = new App({
 
 // "レビュー" を含むメッセージをリッスンします
 app.message('レビュー', async ({ message, say }) => {
+  try {
     if (message.text.indexOf('https://github.com/') !== -1) {
       const messageUser = reviewers.indexOf(message.user)
       reviewers.splice(messageUser, 1)
+      reviewers = activeReviwers(reviewers)
       const firstReviewerNumber = Math.floor(Math.random() * reviewers.length)
       const firstReviewer = reviewers[firstReviewerNumber]
       reviewers.splice(firstReviewerNumber, 1)
       const secondReviewerNumber = Math.floor(Math.random() * reviewers.length)
       const secondReviewer = reviewers[secondReviewerNumber]
-
-      await say({text: `レビューお願いいたします。 first: <@${firstReviewer}>, second: <@${secondReviewer}>!`, thread_ts: message.ts})
+      if (secondReviewer == undefined) {
+        await say({text: `レビューお願いいたします。 first: <@${firstReviewer}>, second: <@${secondReviewer}>!`, thread_ts: message.ts})
+      } else {
+        await say({text: `今はアクティブなユーザーがいないから、時間が立ってからレビューを投げてね`, thread_ts: message.ts})
+      }
     }
+  }
+
+  catch (error) {
+    await say({text: `エラーが発生したよ: ${error}`, thread_ts: message.ts})
+  }
 });
 
 module.exports.handler = async (event, context, callback) => {
@@ -29,12 +39,21 @@ module.exports.handler = async (event, context, callback) => {
   return handler(event, context, callback);
 }
 
-// GitHubでユーザーを管理をする
-// ファイルはServerless Frameworkでlambdaにデプロイをする仕組みを作成する。
-// slackに存在するユーザー名を登録する。
+// UserIDのリスト
 let reviewers = [
   'user1',
   'user2',
   'user3',
   'user4',
 ];
+
+function activeReviwers(reviewers) {
+  return reviewers.map((userId, _) => {
+    let result = app.client.users.getPresence({
+      user: userId
+    });
+    if (result.presence == 'active') {
+      return userId;
+    }
+  });
+}
